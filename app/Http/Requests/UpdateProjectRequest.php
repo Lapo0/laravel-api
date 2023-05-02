43223e1e2caf8286f551c8a0e5645ac5cf3e9769
Lapo0 <lapo.gini.01@gmail.com>
2023-05-02 15:20:54 +0200

    setup repo laravel-api

---
+++ app/Http/Requests/UpdateProjectRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateProjectRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'title' => [
+                'required',
+                'max:150',
+                Rule::unique('projects', 'title')->ignore($this->project)
+            ],
+            'client' => 'required|max:150',
+            'url' => 'required|max:255',
+            'description' => 'nullable|string',
+            'type_id' => 'nullable|exists:types,id',
+            'technologies' => 'exists:technologies,id',
+        ];
+    }
+}
